bd7c6e4f3f06c67da0a5bd3211a53613bdefdd0e
Polly Yospan <porgroovy@gmail.com>
2021-04-30 23:11:11 -0400

    try deploying

---
+++ api/welcome/welcome-router.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.status(200).json({
+    message: `Welcome ${process.env.COHORT}`,
+    fact: process.env.FUN_FACT || "I have no fun facts",
+    port: process.env.PORT,
+  });
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const server = require("./api/server");
+const cors = require("cors");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // START YOUR SERVER HERE
+server.use(cors());
 
 server.listen(port, () => {
   console.log("Server started at localhost:5000");
